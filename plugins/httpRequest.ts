755003bc1a2ade996b2b95a89384c801928239d5
Bupphakon31 <bupphakon.nak31@gmail.com>
2026-01-13 13:52:44 +0700

    feat: add common file

---
+++ plugins/httpRequest.ts
@@ -0,0 +1,9 @@
+import { request } from '@playwright/test';
+
+class HttpRequest {
+    public async createContextHTTPRequest(options: Record<string, any>) {
+        return await request.newContext({...options, ...{failOnStatusCode: false, ignoreHTTPSErrors: true}});
+    }
+}
+
+export const onHTTPRequest: HttpRequest = new HttpRequest();
